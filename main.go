ad2bb7337189711b254f06d886f720521702d85c
yet.another.artem <an.another.artem@gmail.com>
2025-07-18 10:47:37 +0300

    fix: used 'strconv.Itoa' fuction instead of the 'string()' one

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -10,7 +11,7 @@ func main() {
 	dir := os.Args[1]
 	copyright_holder := os.Args[2]
 	current_year := time.Now().Year()
-	license := "Copyright" + copyright_holder + string(current_year)
+	license := "Copyright " + copyright_holder + " " + strconv.Itoa(current_year)
 	fmt.Println("Copyright will be injected in the following file or directory:", dir)
 	if err := InjectLicenses(dir, license); err != nil {
 		fmt.Println("Oh, there is an error:", err)
